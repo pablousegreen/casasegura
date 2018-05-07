5b4bc3f73259dceba43acb71ad359f9262a33f4f
brian.monroy <brayant.mejia@intelmas.com>
2018-05-07 14:48:48 -0500

    Configuracion Baisca

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBQS7zdZdNwgk_3hIYP9kSN_l25vKDa3HI",
+    authDomain: "micasasegura-3afbb.firebaseapp.com",
+    databaseURL: "https://micasasegura-3afbb.firebaseio.com",
+    projectId: "micasasegura-3afbb",
+    storageBucket: "micasasegura-3afbb.appspot.com",
+    messagingSenderId: "218746316448"
+  }
 };
 
 /*
